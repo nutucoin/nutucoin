03ebc9af71c93a4979390a98b7937aef3c91f4f6
Nutucoin <37125859+nutucoin@users.noreply.github.com>
2018-05-16 16:09:35 -0400

    fixed big amount  payments

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -25,7 +25,7 @@ namespace parameters {
 const uint64_t DIFFICULTY_TARGET                             = 240; // seconds
 const uint64_t CRYPTONOTE_MAX_BLOCK_NUMBER                   = 500000000;
 const size_t   CRYPTONOTE_MAX_BLOCK_BLOB_SIZE                = 500000000;
-const size_t   CRYPTONOTE_MAX_TX_SIZE                        = 1000000000000;
+const size_t   CRYPTONOTE_MAX_TX_SIZE                        = 1000000000000000;
 const uint64_t CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX       = 0x1c3a82; // addresses start with "N"
 const size_t   CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW          = 10;
 const uint64_t CRYPTONOTE_BLOCK_FUTURE_TIME_LIMIT            = DIFFICULTY_TARGET * 7;
